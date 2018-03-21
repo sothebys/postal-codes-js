585272511ba9fdf0064517088fa7659c53c2131f
kristinn93 <kristinnj13@ru.is>
2018-03-21 11:08:31 +0000

    test 3

--- formats-web.js
+++ formats-web.js
@@ -49,15 +49,7 @@ formats["SM.json"] = require("./formats/SM.json");
 formats["SO.json"] = require("./formats/SO.json");
 formats["SZ.json"] = require("./formats/SZ.json");
 formats["TC.json"] = require("./formats/TC.json");
-formats["US.json"] = {
-  Description: "US : NNNNN[-NNNN]",
-  RedundantCharacters: " -",
-  ValidationRegex: "^[0-9]{5}([0-9]{4})?$",
-  TestData: {
-    Valid: ["12345", "12345-7689"],
-    Invalid: ["x1231s", "1231sd", "1010101010"]
-  }
-};
+formats["US.json"] = require("./formats/US.json");
 formats["VA.json"] = require("./formats/VA.json");
 formats["VC.json"] = require("./formats/VC.json");
 formats["VE.json"] = require("./formats/VE.json");
